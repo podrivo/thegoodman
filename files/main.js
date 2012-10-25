f22719ed7b53c26586decb9307fb4a6d9700bd60
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2012-10-25 05:01:44 -0200

    Fullscreen mode

--- files/main.js
+++ files/main.js
@@ -1,10 +1,18 @@
 /*! the good man */
 window.jQuery || document.write('<script src="files/jquery.1.8.2.min.js"><\/script>')
 
+/*! jquery.fullscreen 1.1.0 / https://github.com/kayahr/jquery-fullscreen-plugin */
+function d(b){var c,a;if(!this.length)return this;c=this[0];c instanceof Document?(a=c,c=a.documentElement):a=c.ownerDocument;if(null==b){if(!a.cancelFullScreen&&!a.webkitCancelFullScreen&&!a.mozCancelFullScreen)return null;b=!!a.fullScreen||!!a.webkitIsFullScreen||!!a.mozFullScreen;return!b?b:a.fullScreenElement||a.webkitCurrentFullScreenElement||a.mozFullScreenElement||b}b?(b=c.requestFullScreen||c.webkitRequestFullScreen||c.mozRequestFullScreen)&&b.call(c):(b=a.cancelFullScreen||a.webkitCancelFullScreen||
+a.mozCancelFullScreen)&&b.call(a);return this}jQuery.fn.fullScreen=d;jQuery.fn.toggleFullScreen=function(){return d.call(this,!d.call(this))};var e,f,g;e=document;e.webkitCancelFullScreen?(f="webkitfullscreenchange",g="webkitfullscreenerror"):e.mozCancelFullScreen?(f="mozfullscreenchange",g="mozfullscreenerror"):(f="fullscreenchange",g="fullscreenerror");document.addEventListener(f,function(){jQuery(document).trigger(new jQuery.Event("fullscreenchange"))},!0);
+document.addEventListener(g,function(){jQuery(document).trigger(new jQuery.Event("fullscreenerror"))},!0);
+
 /*! random enemys */
 $(document).ready(function() {
 	$('.enemys').find('p').each(function() {
 		$(this).css('top', Math.floor((Math.random())*100) + '%');
 		$(this).css('left', Math.floor((Math.random())*100) + '%');
+	});
+	$(".fs").toggle($(document).fullScreen() != null).click(function() {
+		$(document).toggleFullScreen()
 	});
 });

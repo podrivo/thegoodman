f510c8ba59850f5914be804d69a73e6a586e2401
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2012-11-09 00:39:46 -0200

    More scenes, about.html and credits.html

--- files/main.js
+++ files/main.js
@@ -1,5 +1,4 @@
 /*! The Good Man */
-
 /*! jquery.fullscreen 1.1.0 / https://github.com/kayahr/jquery-fullscreen-plugin */
 function d(b){var c,a;if(!this.length)return this;c=this[0];c instanceof Document?(a=c,c=a.documentElement):a=c.ownerDocument;if(null==b){if(!a.cancelFullScreen&&!a.webkitCancelFullScreen&&!a.mozCancelFullScreen)return null;b=!!a.fullScreen||!!a.webkitIsFullScreen||!!a.mozFullScreen;return!b?b:a.fullScreenElement||a.webkitCurrentFullScreenElement||a.mozFullScreenElement||b}b?(b=c.requestFullScreen||c.webkitRequestFullScreen||c.mozRequestFullScreen)&&b.call(c):(b=a.cancelFullScreen||a.webkitCancelFullScreen||
 a.mozCancelFullScreen)&&b.call(a);return this}jQuery.fn.fullScreen=d;jQuery.fn.toggleFullScreen=function(){return d.call(this,!d.call(this))};var e,f,g;e=document;e.webkitCancelFullScreen?(f="webkitfullscreenchange",g="webkitfullscreenerror"):e.mozCancelFullScreen?(f="mozfullscreenchange",g="mozfullscreenerror"):(f="fullscreenchange",g="fullscreenerror");document.addEventListener(f,function(){jQuery(document).trigger(new jQuery.Event("fullscreenchange"))},!0);
@@ -7,55 +6,39 @@ document.addEventListener(g,function(){jQuery(document).trigger(new jQuery.Event
 
 $(document).ready(function() {
 
-	/*! toggle fullscreen */
+	/* toggle fullscreen */
 	$('.fs').toggle($(document).fullScreen() != null).click(function() {
 		$(document).toggleFullScreen()
 	});
 
-	/*! play */
+	/* play */
 	var audio = $('audio')[0];
 	var animation = $('article');
 	$('.play p').click(function() {
-		$('h1,footer').addClass('hide');
+		$('h1, .footer').addClass('hide');
 		$('.play').fadeOut(900);
-		$('.alert').fadeIn().addClass('sup');
+		$('aside').fadeIn().addClass('sup').css('display','table');
 	});
 	$('.go').click(function() {
 		audio.play();
 		animation.css('display','block');
-		$('.alert').removeClass('sup').addClass('hide');
+		$('aside').removeClass('sup').addClass('hide');
+		$('body').addClass('bg');
+		
+		/* vanish */
+		setTimeout(function(){$('h2, h3, .enemys').addClass('vanish');},10000);
+		setTimeout(function(){$('.good').addClass('vanish');},13200);
+		setTimeout(function(){$('.plus em, .plus b').addClass('vanish');},17200);
 	});
 
-	/*! About */
-	$('footer a:nth-child(1)').click(function() {
-		$('header').fadeOut(900);
-		$('.credits').removeClass('sup').addClass('hide').fadeOut('slow');
-		$('.about').removeClass('hide').fadeIn().addClass('sup');
-	});
-	$('.back').click(function() {
-		$('.about').removeClass('sup').addClass('hide').fadeOut('slow');
-		$('header').fadeIn(900);
-	});
+	
 
-	/*! Credits */
-	$('footer a:nth-child(2)').click(function() {
-		$('header').fadeOut(900);
-		$('.about').removeClass('sup').addClass('hide').fadeOut('slow');
-		$('.credits').removeClass('hide').fadeIn().addClass('sup');
-	});
-	$('.back').click(function() {
-		$('.about, .credits').removeClass('sup').addClass('hide').fadeOut('slow');
-		$('header').fadeIn(900);
-	});
-
-	if(location.href.search('#about') > 0) $('footer a:nth-child(1)').click();
-	if(location.href.search('#credits') > 0) $('footer a:nth-child(2)').click();
-
-	/*! random enemys */
+	/* random enemys */
 	$('.enemys').find('p').each(function() {
 		$(this).css('top', Math.floor((Math.random())*100) + '%');
 		$(this).css('left', Math.floor((Math.random())*100) + '%');
 	});
+	
 });
 
 $(window).load(function() {

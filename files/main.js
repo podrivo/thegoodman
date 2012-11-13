ffb0f1cf9a38e33e53f45ad4f7654981cb55990c
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2012-11-12 22:27:09 -0200

    other tweaks

--- files/main.js
+++ files/main.js
@@ -1,5 +1,5 @@
 /*! The Good Man */
-/*! jquery.fullscreen 1.1.0 / https://github.com/kayahr/jquery-fullscreen-plugin */
+/*! jquery.fullscreen 1.1.0 - http://github.com/kayahr/jquery-fullscreen-plugin */
 function d(b){var c,a;if(!this.length)return this;c=this[0];c instanceof Document?(a=c,c=a.documentElement):a=c.ownerDocument;if(null==b){if(!a.cancelFullScreen&&!a.webkitCancelFullScreen&&!a.mozCancelFullScreen)return null;b=!!a.fullScreen||!!a.webkitIsFullScreen||!!a.mozFullScreen;return!b?b:a.fullScreenElement||a.webkitCurrentFullScreenElement||a.mozFullScreenElement||b}b?(b=c.requestFullScreen||c.webkitRequestFullScreen||c.mozRequestFullScreen)&&b.call(c):(b=a.cancelFullScreen||a.webkitCancelFullScreen||
 a.mozCancelFullScreen)&&b.call(a);return this}jQuery.fn.fullScreen=d;jQuery.fn.toggleFullScreen=function(){return d.call(this,!d.call(this))};var e,f,g;e=document;e.webkitCancelFullScreen?(f="webkitfullscreenchange",g="webkitfullscreenerror"):e.mozCancelFullScreen?(f="mozfullscreenchange",g="mozfullscreenerror"):(f="fullscreenchange",g="fullscreenerror");document.addEventListener(f,function(){jQuery(document).trigger(new jQuery.Event("fullscreenchange"))},!0);
 document.addEventListener(g,function(){jQuery(document).trigger(new jQuery.Event("fullscreenerror"))},!0);
@@ -36,7 +36,7 @@ $(document).ready(function() {
 	$('.go').click(function() {
 		audio.play();
 		animation.css('display','block');
-		$('aside').removeClass('sup').addClass('hide');
+		$('aside').removeClass('sup').addClass('hide').fadeOut(900);
 		$('body').addClass('bg');
 		
 		/* vanish */

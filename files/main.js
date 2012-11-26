37d0c14a324cb5fd377f5eccece135370fdd07a1
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2012-11-26 12:40:25 -0200

    added fire, flames, fight the fire, smash n claw;

--- files/main.js
+++ files/main.js
@@ -24,14 +24,20 @@ $(document).ready(function() {
 
 		/* vanish */
 		if ($('body').is('.nosub')){$('footer').remove();};
-
 		setTimeout(function(){$('header, nav, h2, h3, .enemys').remove();},9500);
-	setTimeout(function(){$('.good').remove();},13200);
-	setTimeout(function(){$('.plus em, .plus b').remove();},17600);
-	setTimeout(function(){$('footer').remove();},126000);
-	setTimeout(function(){$('body').removeClass('cursor');},126000);
+		setTimeout(function(){$('.good').remove();},13200);
+		setTimeout(function(){$('.plus em, .plus b').remove();},20000);
+		setTimeout(function(){$('.plus').remove();},22500);
+		setTimeout(function(){$('.fire').remove();},27000);
+		setTimeout(function(){$('.flames').remove();},30000);
+		setTimeout(function(){$('.smash').remove();},37800);
+		setTimeout(function(){$('.claw').remove();},47000);
+		
+		setTimeout(function(){$('footer').remove();},126000);
+		setTimeout(function(){$('body').removeClass('cursor');},126000);
 	});
 
+	
 	/* toggle fullscreen class */
 	$('.fs').click(function(){$('body').toggleClass('full');});
 
@@ -44,9 +50,18 @@ $(document).ready(function() {
 		$(this).css('top', Math.floor((Math.random())*100) + '%');
 		$(this).css('left', Math.floor((Math.random())*100) + '%');
 	});
+
+	/* fight the fire */
+	$('.fight').find('p').each(function() {
+		$(this).css('top', Math.floor((Math.random())*100) + '%');
+		$(this).css('left', Math.floor((Math.random())*100) + '%');
+	});
 });
 
 $(window).load(function() {
 	$('.play p').fadeIn(1200);
 	$('.play span').fadeOut(700);
 });
+window.addEventListener('load', function() {
+    new FastClick(document.body);
+}, false);

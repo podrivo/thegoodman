3938f3c7d68ce8657ba4a61d871888ae64ce254a
lucasfranco <git@lucasfranco.com.br>
2012-12-05 12:17:46 -0200

    Boas praticas JS e admin no Facebook

--- files/main.js
+++ files/main.js
@@ -1,13 +1,16 @@
 /*! The Good Man */
 $(document).ready(function() {
+	var audio = $('audio')[0],
+		animation = $('article'),
+		body = $('body'),
+		footer = $('footer'),
+		fs = $('.fs');
 
 	/* toggle fullscreen */
-	$('.fs').toggle($(document).fullScreen() != null).click(function() {$(document).toggleFullScreen()});
+	fs.toggle($(document).fullScreen() != null).click(function() {$(document).toggleFullScreen()});
 
 	/* play */
-	var audio = $('audio')[0];
-	var animation = $('article');
-	$('.play p').click(function() {
+	$('.play').find('p').click(function() {
 		$('h1').addClass('tchau');
 		$('.play, nav, .share').fadeOut(900);
 		$('aside').fadeIn().addClass('sup').css('display','table');
@@ -16,16 +19,16 @@ $(document).ready(function() {
 		audio.play();
 		animation.css('display','block');
 		$('aside').removeClass('sup').addClass('tchau').fadeOut(900);
-		$('body').addClass('bg').addClass('cursor');
-		$('footer').fadeIn('fast');
-		
+		body.addClass('bg').addClass('cursor');
+		footer.fadeIn('fast');
+
 		/* remove cursor in fullscreen */
-		if ($('body').is('.full')){$(this).addClass('cursor');};
-	
+		if (body.is('.full')){$(this).addClass('cursor');};
+
 		/* vanish animation */
 		setTimeout(function(){$('header, h2, h3, .enemys').remove();},9500);
 		setTimeout(function(){$('.good').remove();},13200);
-		setTimeout(function(){$('.plus q').remove();},19000);
+		setTimeout(function(){$('.plus').find('q').remove();},19000);
 		setTimeout(function(){$('.plus').remove();},23500);
 		setTimeout(function(){$('.fire').remove();},27000);
 		setTimeout(function(){$('.flames').remove();},36000);
@@ -43,46 +46,56 @@ $(document).ready(function() {
 		setTimeout(function(){$('.am, .fly').remove();},126500);
 		setTimeout(function(){$('.box').remove();},127000);
 		setTimeout(function(){$('.explode').remove();},132000);
-		
+
 		/* vanish animation */
-		if ($('body').is('.nosub')){$('footer').remove();};
+		if (body.is('.nosub')){footer.remove();};
 		setTimeout(function(){$('footer, .progress').remove();},127500);
-		setTimeout(function(){$('body').removeClass('cursor').removeClass('bg');},126000);
+		setTimeout(function(){body.removeClass('cursor').removeClass('bg');},126000);
 		setTimeout(function(){$('nav, .share').fadeIn(900);},130000);
 
 		/* particles */
-		setTimeout(function(){$('.part').hide().append(
-			'<div><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p></div><div><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p></div><div><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p></div><div><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p></div><div><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p></div><div><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p></div><div><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p></div><div><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p><p><b></b></p></div>'
-		).fadeIn(1600);},131000);
+		var toappend = '', i = j = 0;
+		for(; i < 8; i++) {
+			toappend += '<div>';
+			for(; j < 30; j++) {
+				toappend += '<p><b></b></p>';
+			}
+			toappend += '</div>';
+		}
+		setTimeout(function(){$('.part').hide().append(toappend).fadeIn(1600);},131000);
 	});
 
 	/* toggle fullscreen class */
-	$('.fs').click(function(){$('body').toggleClass('full');});
+	fs.click(function(){body.toggleClass('full');});
 
 	/* toggle sub */
 	$('.st em, .fs em').click(function() {$(this).toggleClass('on');});
-	$('.st em').click(function() {$('body').toggleClass('sub').toggleClass('nosub');});
+	$('.st').find('em').click(function() {body.toggleClass('sub').toggleClass('nosub');});
 
 	/* random enemys */
 	$('.enemys').find('p').each(function() {
-		$(this).css('top', Math.floor((Math.random())*100) + '%');
-		$(this).css('left', Math.floor((Math.random())*100) + '%');
+		var that = $(this);
+		that.css('top', Math.floor((Math.random())*100) + '%');
+		that.css('left', Math.floor((Math.random())*100) + '%');
 	});
 
 	/* fight the fire */
 	$('.fight').find('p').each(function() {
-		$(this).css('top', Math.floor((Math.random())*100) + '%');
-		$(this).css('left', Math.floor((Math.random())*100) + '%');
+		var that = $(this);
+		that.css('top', Math.floor((Math.random())*100) + '%');
+		that.css('left', Math.floor((Math.random())*100) + '%');
 	});
 
 	/* virus */
 	$('.virus').find('p').each(function() {
-		$(this).css('top', Math.floor((Math.random())*100) + '%');
-		$(this).css('left', Math.floor((Math.random())*100) + '%');
+		var that = $(this);
+		that.css('top', Math.floor((Math.random())*100) + '%');
+		that.css('left', Math.floor((Math.random())*100) + '%');
 	});
 });
 
 $(window).load(function() {
-	$('.play p').fadeIn(1200);
-	$('.play span').fadeOut(700);
+	var play = $('.play');
+	play.find('p').fadeIn(1200);
+	play.find('span').fadeOut(700);
 });

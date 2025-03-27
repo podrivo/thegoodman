1cd4f69955a6b9b8670c6782975f426e48da3d5f
Pedro Ivo Hudson <ivo.pedro@gmail.com>
2025-03-26 23:38:03 -0300

    remove imagemin from Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,16 +50,16 @@ module.exports = function(grunt) {
       }
     },
 
-    imagemin: {
-      img: {
-        files: [{
-          expand: true,
-          cwd: 'assets/img/',
-          src: ['**/*.{png,jpg,gif}'],
-          dest: 'assets/img/'
-        }]
-      }
-    },
+    // imagemin: {
+    //   img: {
+    //     files: [{
+    //       expand: true,
+    //       cwd: 'assets/img/',
+    //       src: ['**/*.{png,jpg,gif}'],
+    //       dest: 'assets/img/'
+    //     }]
+    //   }
+    // },
 
     clean: {
       noneed: {
@@ -87,7 +87,8 @@ module.exports = function(grunt) {
       },
       images: {
         files: ['assets/img/**/*.{png,jpg,gif}', 'assets/img/*.{png,jpg,gif}'],
-        tasks: ['imagemin'],
+        // tasks: ['imagemin'],
+        tasks: [],
         options: {
           spawn: false
         }
@@ -105,13 +106,13 @@ module.exports = function(grunt) {
   // 3. Where we tell Grunt we plan to use this plug-in.
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  // grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-autoprefixer');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-  grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'autoprefixer', 'imagemin', 'clean']);
-
+  // grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'autoprefixer', 'imagemin', 'clean']);
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'autoprefixer', 'clean']);
 };
